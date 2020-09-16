35101cde7417d0b42212b01d0185f893d219cb00
Ariel Nowak <ariel.nowak90@gmail.com>
2020-09-16 10:07:13 +0200

    with firebase, secret

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,17 +1,10 @@
 import app from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
-
+import React from "react";
 
 const config = {
-    apiKey: "AIzaSyCJ-HtqbKRnKtjr471bp-k2veHvGQAiWQc",
-    authDomain: "oddajubrania.firebaseapp.com",
-    databaseURL: "https://oddajubrania.firebaseio.com",
-    projectId: "oddajubrania",
-    storageBucket: "oddajubrania.appspot.com",
-    messagingSenderId: "548515526156",
-    appId: "1:548515526156:web:0739b8c1742e1255ae6029",
-    measurementId: "G-B0795HMPEY"
+
 };
 
 
